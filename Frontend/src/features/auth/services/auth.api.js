6e080cae7b1f057754a5e59994550398492926db
vishakhaa-malviya <vishakhamalviya679@gmail.com>
2026-03-11 22:47:43 +0530

    update api base url for production

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-generated-mern-stack-application.onrender.com",
     withCredentials: true
 });
 
